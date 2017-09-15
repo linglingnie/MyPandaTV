21bed1f68706d66ad75aa5df3b0148ec0c06aac4
Yuan <1986564257@qq.com>
2017-09-15 14:34:18 +0800

    home 第三次

---
+++ app/src/main/java/com/pandatv/ui/home/adapter/VideoAdapter.java
@@ -0,0 +1,43 @@
+package com.pandatv.ui.home.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Yuan on 2017/9/15.
+ */
+
+public class VideoAdapter extends BaseAdapter {
+    private Context context;
+    private List<Object> list;
+
+    public VideoAdapter(Context context, List<Object> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        return null;
+    }
+}
---
+++ app/src/main/java/com/pandatv/ui/home/bean/BroadcastBean.java
@@ -0,0 +1,8 @@
+package com.pandatv.ui.home.bean;
+
+/**
+ * Created by Yuan on 2017/9/15.
+ */
+
+public class BroadcastBean {
+}
