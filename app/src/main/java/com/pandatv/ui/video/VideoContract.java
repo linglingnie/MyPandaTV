5169a5309d3068ebc8949cdd5e94e8680254a000
天逸100 <1096260574@qq.com>
2017-09-21 21:16:14 +0800

    滚滚视频第四次提交

--- app/src/main/java/com/pandatv/ui/video/VideoContract.java
+++ app/src/main/java/com/pandatv/ui/video/VideoContract.java
@@ -2,6 +2,7 @@ package com.pandatv.ui.video;
 
 import com.pandatv.base.IBasePresenter;
 import com.pandatv.base.IBaseView;
+import com.pandatv.ui.video.user.PandaCulture;
 
 /**
  * Created by 天逸100 on 2017/9/15.
